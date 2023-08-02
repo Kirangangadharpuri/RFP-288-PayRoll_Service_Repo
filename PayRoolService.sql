590bc2efa3f456e535388810305b9706dd3e3cf3
Kiran <kiranpuri532@gmail.com>
2023-08-02 12:58:23 +0530

    [Kiran]Add:\ Insert values in eployee payroll table using MSSQL client

--- PayRoolService.sql
+++ PayRoolService.sql
@@ -5,3 +5,16 @@ USE Payroll_Service;
 --UC2
 CREATE TABLE Employee_PayRoll(ID int Primary key Identity(101,1), Name varchar(50), Salary int, StartDate Date);
 SELECT * FROM Employee_PayRoll;
+
+--uc3
+INSERT INTO Employee_PayRoll(Name,Salary,StartDate)
+VALUES('Prasad Deshamukh',30000,'2018-01-10'),
+	  ('Bill',45000,'2019-07-05'),
+	  ('Eleza',55000,'2018-10-22'),
+	  ('Lusi',33000,'2016-12-30'),
+	  ('Max',60000,'2010-04-25'),
+	  ('Sunita',23000,'2020-02-19'),
+	  ('Rahul',20000,'2021-01-29'),
+	  ('Sneha',42000,'2018-02-17'),
+	  ('Kiran',40000,'2022-05-20'),
+	  ('Cen',50000,'2020-08-10');
