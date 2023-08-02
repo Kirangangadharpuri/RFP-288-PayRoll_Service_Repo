1490ea2aa821f3fc4f9600d23d21e34e7cabb2fc
Kiran <kiranpuri532@gmail.com>
2023-08-02 12:34:17 +0530

    [Kiran]Add:\ create eployee payroll table using MSSQL client

--- PayRoolService.sql
+++ PayRoolService.sql
@@ -1,2 +1,7 @@
+--UC-1
 CREATE DATABASE Payroll_Service;
 USE Payroll_Service;
+
+--UC2
+CREATE TABLE Employee_PayRoll(ID int Primary key Identity(101,1), Name varchar(50), Salary int, StartDate Date);
+SELECT * FROM Employee_PayRoll;
